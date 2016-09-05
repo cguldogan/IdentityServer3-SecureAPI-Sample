816bae2fdae0cbed2fd4caed367d5a97f7d48e01
cguldogan <cguldogan@gmail.com>
2016-09-05 23:21:39 +0300

    Init

---
+++ src/SecureAPISample/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SecureAPISample
+{
+    public class ValuesController : ApiController
+    {
+        
+        public string Get()
+        {
+            return "hi anonymous";
+        }
+
+
+        [Authorize] //You need to get token to call this method
+        public string Get(int id)
+        {
+            return id.ToString();
+        }
+
+    }
+}
---
+++ src/SecureAPISample/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using IdentityServer3.AccessTokenValidation;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using SecureAPISample.App_Start;
+
+[assembly: OwinStartup(typeof(SecureAPISample.Startup))]
+
+namespace SecureAPISample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
+            {
+                Authority = "http://10.10.27.36:4044", //Your Identity Server address
+
+            });
+
+            var config = new HttpConfiguration();
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+
+        }
+    }
+}
